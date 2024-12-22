4c20623363c3935d3b3e053ca751d51372c65f36
Ayman <ayman@gmail.com>
2024-12-22 21:16:20 +0100

    new title

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BLACK&WHITE',
+      title: 'BLACK&WHITE!',
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
